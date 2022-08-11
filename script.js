ca49c625299c7c68fdfd04c4bf37168642f9359c
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-08-11 18:26:32 -0300

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+/*Bandeiras*/
+const Brasil = document.querySelector('.portugues')
+const EstadosUnidos = document.querySelector('.ingles')
+const Espanha = document.querySelector('.espanhol')
+
+Brasil.addEventListener('click', () => {
+    console.log('br')
+})
+
+EstadosUnidos.addEventListener('click', () => {
+    document.querySelector('.titulo-BR').innerHTML = 'espanha only'
+})
+
+Espanha.addEventListener('click', () => {
+    console.log('es')
+})
